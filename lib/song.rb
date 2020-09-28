b66deeba326bf24894581dd3f487e36372420c76
Nick Nobile <nickcnobile@gmail.com>
2020-09-28 10:41:28 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,13 +1,11 @@
 class Song
   attr_accessor :name, :artist, :genre
 
-  @@album_count = 0
+  @@count = 0
   @@genres = []
 
   def initialize
-    @@album_count += 1
-    @@name
-    @@genre
+    @@count += 1
   end
 
-Song.new(:name, :artist, :genre)
+  
