e4c92c05eb613585a2d439bd90bfb5084a79dd5f
Nick Nobile <nickcnobile@gmail.com>
2020-09-28 12:02:15 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,7 +9,7 @@ class Song
     @name = name
     @artist = artist
     @genre = genre
-    @@artists << artist 
+    @@artists << artist
     @@genres << genre
     @@count += 1
   end
@@ -24,7 +24,7 @@ class Song
 
   def self.genres
   @@genres
-  end 
+  end
 end
 
 Song.count
