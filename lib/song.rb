55f2fa0e0b2ce8103d3ee64fc41723c4353bc19b
Nick Nobile <nickcnobile@gmail.com>
2020-09-28 14:06:54 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,55 @@
+require 'pry'
+
+class Song
+  @@artists = []
+  @@genres = []
+  @@count = 0
+
+  attr_accessor :name, :artist, :genre
+
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+
+    @@artists << artist
+    @@genres << genre
+    @@count += 1
+  end
+
+  def self.count
+   @@count
+  end
+
+  def self.artists
+    @@artists.uniq
+  end
+
+  def self.genres
+    @@genres.uniq
+  end
+
+  def self.genre_count
+    new_genre_count = {}
+    @@genres.each do |genre|
+      if new_genre_count[genre]
+        new_genre_count[genre] += 1
+      else
+        new_genre_count[genre] = 1
+      end
+    end
+    new_genre_count
+  end
+
+  def self.artist_count
+    new_artist_count = {}
+    @@artists.each do |artist|
+      if new_artist_count[artist]
+        new_artist_count[artist] += 1
+      else
+        new_artist_count[artist] = 1
+      end
+    end
+    new_artist_count
+  end
+end
